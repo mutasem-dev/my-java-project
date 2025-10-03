840016f3b0ba9cdb1b56605546e71e89c508061c
mutasem-dev <malheeh@ppu.edu>
2025-10-03 20:21:35 +0300

    add add_doubles function

--- src/main/java/com/example/calculator.java
+++ src/main/java/com/example/calculator.java
@@ -5,6 +5,9 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+    public double add(double a, double b) {
+        return a + b;
+    }
     public int sub(int a, int b) {
 	return a-b;
     }
